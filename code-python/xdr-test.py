49812b8c9f4763acb473a70d213fd4203d7d8809
Brian Munroe <bmunroe@zappos.com>
2010-07-31 12:01:23 -0700

    fixing things

--- code-python/xdr-test.py
+++ code-python/xdr-test.py
@@ -1,9 +1,20 @@
 import xdrlib
 
 p = xdrlib.Packer()
+
 try:
 	p.pack_double(8.01)
 	print p.get_buffer()
 
 except xdrlib.ConversionError, instance:
     print 'packing the double failed:', instance.msg
+
+
+class Person(object):
+    def __init__(self):
+        self.first_name = None
+        self.last_name = None
+
+    def get_first_name(self):
+        return self.first_name
+
