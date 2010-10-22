48c99fdb784227e00f024ebde6796c5ca236ebb7
Brian Munroe <brian.e.munroe@gmail.com>
2010-10-21 21:50:38 -0700

    more contact bridge hacking

--- code-python/google-docs-to-gmail-contacts-bridge.py
+++ code-python/google-docs-to-gmail-contacts-bridge.py
@@ -1,4 +1,4 @@
-#!/bin/python
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 try: 
@@ -12,21 +12,115 @@ import atom.service
 import gdata.spreadsheet
 import atom
 
-def PrintFeed(feed):
-    for i, entry in enumerate(feed.entry):
-        if isinstance(feed, gdata.spreadsheet.SpreadsheetsCellsFeed):
-            print '%s %s\n' % (entry.title.text, entry.content.text)
-        elif isinstance(feed, gdata.spreadsheet.SpreadsheetsListFeed):
-            print '%s %s %s' % (i, entry.title.text, entry.content.text)
-            # Print this row's value for each column (the custom dictionary is
-            # built from the gsx: elements in the entry.) See the description of
-            # gsx elements in the protocol guide.
-            print 'Contents:'
-            for key in entry.custom:
-                print '  %s: %s' % (key, entry.custom[key].text)
-                print '\n',
-        else:
-            print '%s %s\n' % (i, entry.title.text)
+class Contact(object):
+
+    def __init__(self):
+
+        self._first_name = ""
+        self._last_name = ""
+        self._home_address = ""
+        self._home_city = ""
+        self._home_state = ""
+        self._home_zip = ""
+        self._home_phone = ""
+        self._mobile_phone = ""
+        self._work_phone = ""
+        self._home_email = ""
+        self._work_email = ""
+
+    @property
+    def first_name(self):
+        return self._first_name
+    
+    @first_name.setter
+    def first_name(self,value):
+        self._first_name = value
+
+    @property
+    def last_name(self):
+        return self._last_name
+    
+    @last_name.setter
+    def last_name(self,value):
+        self._last_name = value
+
+    @property
+    def home_address(self):
+        return self._home_address
+    
+    @home_address.setter
+    def home_address(self,value):
+        self._home_address = value
+
+    @property
+    def home_city(self):
+        return self._home_city
+    
+    @home_city.setter
+    def home_city(self,value):
+        self._home_city = value
+
+    @property
+    def home_state(self):
+        return self._home_state
+    
+    @home_state.setter
+    def home_state(self,value):
+        self._home_state = value
+
+    @property
+    def home_zip(self):
+        return self._home_zip
+    
+    @home_zip.setter
+    def home_zip(self,value):
+        self._home_zip = value
+
+    @property
+    def full_home_address(self):
+        return self._home_address + ", " + self._home_city + ", " + \
+        self._home_state + ", " + self._home_zip 
+
+    @property
+    def home_phone(self):
+        return self._home_phone
+    
+    @home_phone.setter
+    def home_phone(self,value):
+        self._home_phone = value
+
+    @property
+    def mobile_phone(self):
+        return self._mobile_phone
+    
+    @mobile_phone.setter
+    def mobile_phone(self,value):
+        self._mobile_phone = value
+
+    @property
+    def work_phone(self):
+        return self._work_phone
+    
+    @work_phone.setter
+    def work_phone(self,value):
+        self._work_phone = value
+
+    @property
+    def home_email(self):
+        return self._home_email
+    
+    @home_email.setter
+    def home_email(self,value):
+        self._home_email = value
+
+    @property
+    def work_email(self):
+        return self._work_email
+    
+    @work_email.setter
+    def work_email(self,value):
+        self._work_email = value
+
 
 class ContactBridge(object):
 
@@ -52,12 +146,57 @@ class ContactBridge(object):
 
         self.address_feed = self.s_client.GetListFeed(self.spreadsheet_key,self.worksheet_key) 
 
-    def dump_to_csv(self):
-        PrintFeed(self.address_feed)
+    def get_spreadsheet_contacts(self):
+        contact_list = []
+        
+        for i, entry in enumerate(self.address_feed.entry):
+
+            c = Contact()
+
+            for key in entry.custom:
+
+                if key == 'firstname':
+                    c.first_name = entry.custom[key].text
+                
+                if key == 'lastname':
+                    c.last_name = entry.custom[key].text
+
+                if key == 'homeaddress':
+                    c.home_address = entry.custom[key].text
+
+                if key == 'homecity':
+                    c.home_city = entry.custom[key].text
+
+                if key == 'homestate':
+                    c.home_state = entry.custom[key].text
+
+                if key == 'homezip':
+                    c.home_zip = entry.custom[key].text
+
+                if key == 'homephone':
+                    c.home_phone = entry.custom[key].text
+           
+                if key == 'mobiephone':
+                    c.mobile_phone = entry.custom[key].text
+
+                if key == 'workphone':
+                    c.work_phone = entry.custom[key].text
+
+                if key == 'homeemail':
+                    c.home_email = entry.custom[key].text
+
+                if key == 'workemail':
+                    c.work_email = entry.custom[key].text
+
+            contact_list.append(c)
+   
+        return contact_list
 
 def main():
-    cb1 = ContactBridge(spreadsheet='Addressbook',worksheet='Iphone prep')
-    cb1.dump_to_csv()
+    cb1 = ContactBridge(email='',password='',spreadsheet='Addressbook',worksheet='master')
+    contacts = cb1.get_spreadsheet_contacts()
+    print contacts[11].full_home_address
+    #cb1.import_into_google_contacts(contacts)
 
 if __name__ == '__main__': main()
 
